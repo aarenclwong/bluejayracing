d1c8b4e7814000bc0a11aab3dfc5c74c584a7585
hoybag <dragoonumgt@gmail.com>
2022-10-01 11:39:54 -0400

    simple offset calibration

--- IMU.py
+++ IMU.py
@@ -2,6 +2,7 @@ from mahony_ahrs import Mahony, DEFAULT_SAMPLE_FREQ
 import board
 import adafruit_fxos8700 # accel and magnet
 import adafruit_fxas21002c # Gyro
+import time
 
 class IMU:
     def __init__(self, sample_freq=DEFAULT_SAMPLE_FREQ):
@@ -10,11 +11,47 @@ class IMU:
         self.sensor = adafruit_fxos8700.FXOS8700(self.i2c)
         self.mahony = Mahony(sample_freq)
 
+    def calibrate(self):
+        ax_avg = 0
+        ay_avg = 0
+        az_avg = 0
+        gx_avg = 0
+        gy_avg = 0
+        gz_avg = 0
+        n = 0
+        now = time.time()
+        while time.time() - now < 3:
+            ax, ay, az = self.sensor.accelerometer
+            gx, gy, gz = self.gyro.gyroscope
+            ax_avg += ax
+            ay_avg += ay
+            az_avg += az
+            gx_avg += gx
+            gy_avg += gy
+            gz_avg += gz
+            n += 1
+            time.sleep(0.1)
+
+        self.ax_offset = ax_avg/n
+        self.ay_offset = ay_avg/n
+        self.az_offset = az_avg/n - 9.81
+        self.gx_offset = gx_avg/n
+        self.gy_offset = gy_avg/n
+        self.gz_offset = gz_avg/n
+
+    def adjust(self, gx, gy, gz, ax, ay, az, mx, my, mz):
+        # adjust according to calibrate data
+        return gx - self.gx_offset, gy - self.gy_offset, gz - self.gz_offset, \
+            ax - self.ax_offset, ay - self.ay_offset, az-self.az_offset, \
+            mx, my, mz
+             
+        
+
     def get_raw(self):
         ax, ay, az = self.sensor.accelerometer
         gx, gy, gz = self.gyro.gyroscope
         mx, my, mz = self.sensor.magnetometer
-        return gx, gy, gz, ax, ay, az, mx, my, mz
+        return self.adjust(gx, gy, gz, ax, ay, az, mx, my, mz)
 
     def update(self):
         self.mahony.update(*self.get_raw())
