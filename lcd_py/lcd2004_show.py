17d98175f202febf941a9170a3513e9a2ee2f1a8
Aaren Wong <aarenclwong@gmail.com>
2023-04-22 12:28:30 -0400

    changed LCD1602 to LCD2004

--- lcd_py/lcd2004_show.py
+++ lcd_py/lcd2004_show.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-import LCD1602
+import LCD2004
 import time
 
 def setup():
-	LCD1602.init(0x27, 1)	# init(slave address, background light)
-	LCD1602.write(0, 0, 'Hello, world')
-	LCD1602.write(0, 1, 'IIC/I2C LCD2004')
-	LCD1602.write(0, 2, '20 cols, 4 rows')
-	LCD1602.write(0, 3, 'www.sunfounder.com')
+	LCD2004.init(0x27, 1)	# init(slave address, background light)
+	LCD2004.write(0, 0, 'Hello, world')
+	LCD2004.write(0, 1, 'IIC/I2C LCD2004')
+	LCD2004.write(0, 2, '20 cols, 4 rows')
+	LCD2004.write(0, 3, 'www.sunfounder.com')
 	time.sleep(2)
 
 def destroy():
-	LCD1602.clear()
+	LCD2004.clear()
 
 if __name__ == "__main__":
 	try:
