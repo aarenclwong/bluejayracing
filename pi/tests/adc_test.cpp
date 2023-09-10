aa021c0b2358e38375b80c76dfe7c2701aec21fb
Aaren Wong <aarenclwong@gmail.com>
2023-09-09 22:52:04 -0400

    strain testing

--- pi/tests/adc_test.cpp
+++ pi/tests/adc_test.cpp
@@ -32,7 +32,7 @@ int main(/*int argc, char* argv[]*/) {
     sprintf(err, "Failed to open i2c bus (%c) ", BUS_NAME);
   }
 
-  ADC a = ADC(fd, 2, true);
+  ADC a = ADC(fd, 0, false);
   // ADC a = ADC(fd, 0, false);
   //ADC a = ADC(fd, 3, true);
 
