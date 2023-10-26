22b2dd917d842c76cdda330e1d7f0ae9e4c7380b
Ashley Zhang <ashleyzhang@Ashleys-Laptop.mshome.net>
2023-10-25 20:13:48 -0400

    file to write volts

---
+++ teensy/write_volts/write_volts.ino
@@ -0,0 +1,96 @@
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+#include <SD.h>
+#include <SPI.h>
+
+File myFile;
+
+Adafruit_INA219 ina219;
+
+const int chipSelect = BUILTIN_SDCARD;
+
+void setup(void)
+{
+  Serial.begin(115200);
+  while (!Serial) {
+      // will pause Zero, Leonardo, etc until serial console opens
+      delay(1);
+  }
+    
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(chipSelect)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+  
+  // Initialize the INA219.
+  // By default the initialization will use the largest range (32V, 2A).  However
+  // you can call a setCalibration function to change this range (see comments).
+  if (! ina219.begin()) {
+    Serial.println("Failed to find INA219 chip");
+    while (1) { delay(10); }
+  }
+  // To use a slightly lower 32V, 1A range (higher precision on amps):
+  //ina219.setCalibration_32V_1A();
+  // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
+  ina219.setCalibration_16V_400mA();
+
+  Wire.begin();
+
+  Serial.println("Measuring voltage and current with INA219 ...");
+}
+
+void loop(void) 
+{
+  // open the file. 
+  myFile = SD.open("test.txt", FILE_WRITE);
+  
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to test.txt...");
+    write_volts(myFile);
+	// close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+
+  delay(1000);
+}
+
+void write_volts(File myFile){
+  float shuntvoltage = 0;
+  float busvoltage = 0;
+  float current_mA = 0;
+  float loadvoltage = 0;
+  float power_mW = 0;
+
+  shuntvoltage = ina219.getShuntVoltage_mV();
+  busvoltage = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  power_mW = ina219.getPower_mW();
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
+
+  myFile.print("Bus Voltage:   "); myFile.print(busvoltage); myFile.println(" V");
+  myFile.print("Shunt Voltage: "); myFile.print(shuntvoltage); myFile.println(" mV");
+  myFile.print("Load Voltage:  "); myFile.print(loadvoltage); myFile.println(" V");
+  myFile.print("Current:       "); myFile.print(current_mA); myFile.println(" mA");
+  myFile.print("Power:         "); myFile.print(power_mW); myFile.println(" mW");
+  myFile.println("");
+}
+
+void time_elapsed(int hour, int min, int sec, int milli){
+  total = millis();
+  hour += total / 3.6e+6;
+  total %= 3.6e+6;
+  min += total / 60000;
+  total %= 60000;
+  sec += total /1000;
+  total %= 1000;
+  milli += total;
+  Serial.println(hour)
+}
