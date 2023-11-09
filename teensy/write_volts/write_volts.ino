8e29165698a288d5355ead2c92a5e984d62fa9e7
Ashley Zhang <ashleyzhang@Ashleys-Laptop.mshome.net>
2023-11-08 20:26:58 -0500

    mashed everything together

--- teensy/write_volts/write_volts.ino
+++ teensy/write_volts/write_volts.ino
@@ -1,3 +1,4 @@
+#include <TimeLib.h>
 #include <Wire.h>
 #include <Adafruit_INA219.h>
 #include <SD.h>
@@ -11,25 +12,32 @@ const int chipSelect = BUILTIN_SDCARD;
 
 void setup(void)
 {
-  Serial.begin(115200);
-  while (!Serial) {
-      // will pause Zero, Leonardo, etc until serial console opens
-      delay(1);
-  }
+  SD.begin(chipSelect); // set chip to SD card
+
+  // Serial.begin(115200);
+  // while (!Serial) {
+  //     // will pause Zero, Leonardo, etc until serial console opens
+  //     delay(1);
+  // }
     
-  Serial.print("Initializing SD card...");
+  // Serial.print("Initializing SD card...");
 
   if (!SD.begin(chipSelect)) {
-    Serial.println("initialization failed!");
+    // Serial.println("initialization failed!");
     return;
   }
-  Serial.println("initialization done.");
+  // Serial.println("initialization done.");
+
+  if (rtc_get() < 2000000000){
+    // Serial.println("ITS BEING RESET");
+    rtc_set(2000000000); // set base time (starts counting number of seconds starting at 2 billion)
+  }
   
   // Initialize the INA219.
   // By default the initialization will use the largest range (32V, 2A).  However
   // you can call a setCalibration function to change this range (see comments).
   if (! ina219.begin()) {
-    Serial.println("Failed to find INA219 chip");
+    // Serial.println("Failed to find INA219 chip");
     while (1) { delay(10); }
   }
   // To use a slightly lower 32V, 1A range (higher precision on amps):
@@ -39,24 +47,23 @@ void setup(void)
 
   Wire.begin();
 
-  Serial.println("Measuring voltage and current with INA219 ...");
+  // Serial.println("Measuring voltage and current with INA219 ...");
 }
 
 void loop(void) 
 {
   // open the file. 
-  myFile = SD.open("test.txt", FILE_WRITE);
+  myFile = SD.open("test1.txt", FILE_WRITE);
   
   // if the file opened okay, write to it:
   if (myFile) {
-    Serial.print("Writing to test.txt...");
+    time_elapsed(myFile, 0, 0, 0); // SET START TIME HERE
     write_volts(myFile);
 	// close the file:
     myFile.close();
-    Serial.println("done.");
   } else {
     // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+    myFile.println("error opening test.txt");
   }
 
   delay(1000);
@@ -75,6 +82,7 @@ void write_volts(File myFile){
   power_mW = ina219.getPower_mW();
   loadvoltage = busvoltage + (shuntvoltage / 1000);
 
+  myFile.println("");
   myFile.print("Bus Voltage:   "); myFile.print(busvoltage); myFile.println(" V");
   myFile.print("Shunt Voltage: "); myFile.print(shuntvoltage); myFile.println(" mV");
   myFile.print("Load Voltage:  "); myFile.print(loadvoltage); myFile.println(" V");
@@ -83,14 +91,26 @@ void write_volts(File myFile){
   myFile.println("");
 }
 
-void time_elapsed(int hour, int min, int sec, int milli){
-  total = millis();
-  hour += total / 3.6e+6;
-  total %= 3.6e+6;
-  min += total / 60000;
-  total %= 60000;
-  sec += total /1000;
-  total %= 1000;
-  milli += total;
-  Serial.println(hour)
+void time_elapsed(File myFile, int hour, int min, int sec){
+  int total = rtc_get() - 2000000000;
+  hour += total / 3600;
+  total %= 3600;
+  min += total / 60;
+  total %= 60;
+  sec += total;
+
+  myFile.println("");
+  myFile.print(hour);
+  myFile.print(":");
+  myFile.print(min);
+  myFile.print(":");
+  myFile.print(sec);
+
+  // Serial.println(rtc_get());
+  // Serial.println("");
+  // Serial.print(hour);
+  // Serial.print(":");
+  // Serial.print(min);
+  // Serial.print(":");
+  // Serial.print(sec);
 }
